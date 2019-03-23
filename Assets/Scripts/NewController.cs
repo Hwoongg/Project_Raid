0baca535f3db7a6e349158c199a861b00d5ec560
Hwoongg <gusdnd8536@gmail.com>
2019-03-24 00:54:43 +0900

    Primary Commit
    초회 커밋입니다.
    
    구성은 아래와 같습니다.
    - 테스트용 씬
    - 테스트 오브젝트용 앞 뒷면 스프라이트
    - 초기형 3인칭 카메라 워크, 플레이어 조작 스크립트
    - 새로 개발중인 카메라 워크, 플레이어 조작 스크립트
    - 빌보드 스프라이트 회전 스크립트
    
    설정된 gitignore 파일 내용 이상 없는지에 대한 확인이 필요합니다.

---
+++ Assets/Scripts/NewController.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+// 플레이어의 회전, 이동을 담당하는 컴포넌트 스크립트 입니다.
+// 컨트롤러 모드에 따라 카메라 워크 컴포넌트의 모드도 제어됩니다. 
+//
+
+public class NewController : MonoBehaviour
+{
+    enum Mode
+    {
+        Normal,
+        Jet
+    }
+    Mode mode;
+
+    [SerializeField]
+    Transform CameraTransform;
+    NewTPSCamera TPSCam;
+
+    Transform PlayerTransform;
+    Vector3 MoveVector; // 이동량 벡터
+    Vector3 MoveDirection;
+
+    [SerializeField] float MaxSpeed = 1.0f;
+    [SerializeField] float Acceleration = 0.1f; // 가속도
+    [SerializeField] float Deceleration = 0.5f; // 감속도
+
+    [SerializeField]
+    float XaxisSpeed = 30.0f, YaxisSpeed = 30.0f;
+    float X, Y;
+
+    [SerializeField]
+    float MinAngle = -45f, MaxAngle = 45f;
+
+    private void Awake()
+    {
+        mode = Mode.Normal;
+        MoveVector = Vector3.zero;
+    }
+
+    void Start()
+    {
+        PlayerTransform = GetComponent<Transform>();
+        TPSCam = CameraTransform.GetComponent<NewTPSCamera>();
+    }
+    
+    void Update()
+    {
+        NormalStateControll();
+    }
+
+    // 일반 상태 회전 + 이동 기능입니다.
+    void NormalStateControll()
+    {
+        // 카메라 모드 제어
+        TPSCam.mode = NewTPSCamera.Mode.Jet;
+
+        // 일반 상태 회전
+        NormalStateRotation();
+
+        // 일반 상태 이동
+        NormalStateMovement();
+
+    }
+
+    void NormalStateRotation()
+    {
+        //
+        // 입력부
+        //
+        float v = Input.GetAxis("Mouse X");
+        float h = Input.GetAxis("Mouse Y");
+
+        X += v * XaxisSpeed * Time.deltaTime;
+        Y -= h * YaxisSpeed * Time.deltaTime;
+
+        Y = Mathf.Clamp(Y, MinAngle, MaxAngle);
+
+
+        // 
+        // 처리부
+        //
+        var rot = Quaternion.Euler(Y, X, 0);
+
+        PlayerTransform.rotation = Quaternion.Euler(Y, X + 180f, 0);
+    }
+
+    
+    void NormalStateMovement()
+    {
+        // ////////////////////////////////////////////////////
+        //
+        // 카메라 기준 방향벡터를 이용한 이동변환을 시행합니다.
+        // 전역-모델 좌표계 전환에 유의하여 사용해야 합니다.
+        // 
+        // ////////////////////////////////////////////////////
+
+
+        //
+        // 입력부
+        //
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // 월드 기준 벡터들입니다. 이동 시 모델좌표계로 사상시켜야 함.
+        Vector3 worldY = Input.GetKey(KeyCode.Space) ? new Vector3(0, 1, 0) : new Vector3(0, 0, 0);
+        Vector3 cameraX = CameraTransform.right * x;
+        Vector3 cameraZ = CameraTransform.forward * z;
+
+        Vector3 Movement = cameraX + cameraZ + worldY;
+
+        Movement.Normalize();
+
+        Movement = PlayerTransform.worldToLocalMatrix * Movement;
+
+        MoveVector += Movement * Acceleration * Time.deltaTime;
+
+        MoveVector = MoveVector - (MoveVector * Deceleration * Time.deltaTime);
+
+        PlayerTransform.Translate(MoveVector);
+    }
+
+    void JetStateControll()
+    {
+        //
+        // 입력부
+        // 오브젝트 기준 회전입니다.
+        // 마우스 좌우 : Z축 회전
+        // 마우스 상하 : X축 회전
+        // 키보드 좌우 : Y축 회전
+        // Z축 회전 시 카메라도 같이 회전이 이루어짐.
+        // 이동 자체는 오브젝트의 정면으로만 진행합니다.
+        //
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        
+    }
+}
---
+++ Assets/Scripts/NewTPSCamera.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+// 카메라 워크 스크립트 입니다.
+// 플레이어 머리 뒤의 상대좌표를 추적합니다.
+// 추적 방식에 따른 모드와 그에 따른 추적 방식 함수가 정의되어 있습니다.
+//
+
+public class NewTPSCamera : MonoBehaviour
+{
+
+    public enum Mode
+    {
+        None,
+        Normal,
+        Jet,
+        Free
+    }
+    [HideInInspector] public Mode mode;
+
+    Transform FollowTarget; // 추적 타겟
+    PlayerControll playerControll;
+
+    private Transform CamTransform;
+
+
+    private void Awake()
+    {
+        CamTransform = GetComponent<Transform>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+        mode = Mode.Free;
+
+    }
+
+
+    void Start()
+    {
+        FollowTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        playerControll = FollowTarget.GetComponent<PlayerControll>();
+    }
+    
+    
+
+    private void LateUpdate()
+    {
+        switch(mode)
+        {
+            case Mode.None:
+                break;
+
+            case Mode.Normal:
+                NormalStateWork();
+                break;
+                
+            case Mode.Jet:
+                JetStateWork();
+                break;
+
+            case Mode.Free:
+                FreeStateWork();
+                break;
+        }
+    }
+
+    // 기본 모드 카메라 워크입니다.
+    void NormalStateWork()
+    {
+        var newPos = FollowTarget.localToWorldMatrix * new Vector4(0.0f, 3.0f, -3.5f, 1);
+
+        CamTransform.position = newPos;
+
+        CamTransform.LookAt(FollowTarget);
+
+        //CamTransform.forward = Vector3.Normalize(FollowTarget.position - CamTransform.position);
+    }
+
+    // 추적모드 카메라 워크입니다.
+    void JetStateWork()
+    {
+        var newPos = FollowTarget.localToWorldMatrix * new Vector4(0.0f, 3.0f, -3.5f, 1);
+
+        CamTransform.position = Vector3.Lerp(CamTransform.position, newPos, Time.fixedDeltaTime * 10.0f);
+        
+        CamTransform.LookAt(FollowTarget);
+
+        // 오브젝트의 회전만큼 회전시킨다.
+        // ...
+        
+
+    }
+
+    // 자유시점 카메라 워크입니다.
+    void FreeStateWork()
+    {
+
+        FreeRotation();
+    }
+
+    // 플레이어를 중심에 놓고 회전합니다.
+    void FreeRotation()
+    {
+        float v = Input.GetAxis("Mouse X");
+        float h = Input.GetAxis("Mouse Y");
+
+        var newPos =
+            Quaternion.AngleAxis(v * Time.deltaTime * 10.0f, Vector3.up)
+            * Quaternion.AngleAxis(h * Time.deltaTime * 10.0f, Vector3.Cross(CamTransform.forward, Vector3.up))
+             * CamTransform.position;
+
+        CamTransform.position = newPos;
+
+        CamTransform.LookAt(FollowTarget);
+
+
+    }
+}
