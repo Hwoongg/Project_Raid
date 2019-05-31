3aabfe8cfca24eb5a32f2c287621d8ecb3f06282
Hwoongg <gusdnd8536@gmail.com>
2019-05-31 14:12:16 +0900

    포탑 관련 스크립트 수정

--- Assets/Scripts/BulletScripts/EyeTurret.cs
+++ Assets/Scripts/BulletScripts/EyeTurret.cs
@@ -2,20 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//
+// 구체형 포탑에 사용되는 스크립트.
+//
+
 public class EyeTurret : MonoBehaviour
 {
+    // //////////////////////////////////////////////
+    //
+    // 눈알 회전에 관련된 변수들
+    //
+    // /////////////////////////////////////////////
+
+    // 회전 속도
     [SerializeField] float RotSpeed = 10.0f;
 
+    // 눈알 오브젝트
     [SerializeField] Transform EyeTransform;
+
+    // 조준 대상
     Transform Target;
+    
+
+    // /////////////////////////////////////////////
+    //
+    // 포탄 발사에 관련된 변수들
+    //
+    // /////////////////////////////////////////////
+
+    Muzzle[] FirePoints; // 발사 지점 오브젝트에 Muzzle 컴포넌트가 있어야 한다
+    float ReloadTime;
+
+
 
     private void Awake()
     {
         Target = GameObject.FindGameObjectWithTag("Player").transform;
+        FirePoints = GetComponentsInChildren<Muzzle>();
     }
     
-
-    // Update is called once per frame
+    
     void Update()
     {
         Quaternion LookDir = Quaternion.LookRotation( Target.position - EyeTransform.position);
@@ -25,5 +51,10 @@ public class EyeTurret : MonoBehaviour
 
         // 대상을 바라보도록 전환
         EyeTransform.rotation = Quaternion.Slerp(EyeTransform.rotation, LookDir, RotSpeed * Time.deltaTime);
+    }
+
+    void Fire()
+    {
+
     }
 }
